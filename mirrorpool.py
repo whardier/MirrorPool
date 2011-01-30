bd61ba2a69aa2519053c9eab4d82791b290a6bba
Shane R. Spencer <shane@bogomip.com>
2011-01-29 15:39:38 -0900

    branching off to whardier

--- mirrorpool.py
+++ mirrorpool.py
@@ -13,6 +13,11 @@ if __name__ == '__main__':
   import asyncore
   import pprint
 
-  pprint.pprint(sys.modules)
+  for module in sys.modules:
+    try:
+      t = getattr(sys.modules[module], 'args')
+      print module, t
+    except:
+      pass
 
   asyncore.loop()
--- settings.py
+++ settings.py
@@ -1,14 +1,12 @@
-DATASTORE = "redis"
-
-DATASTORE_URI = "redis://127.0.1.2:6379"
+DATASTORE = ("redis", {'host': '127.0.1.2', 'port': 6379})
 
 INTERFACES = (
-  'cache.icp', #Use Defaults 0.0.0.0:3131
+  'cache.icp', 
   ('proxy.http', {'port': 8080, 'interface': '127.0.1.2'}),
 )
 
 PROXIES = (
-  'apple.software.updates',
+  ('apple.software.updates', {'watch': True}),
 # 'apple.software.appstore',
 # 'apt.software.pool',
 # 'rpm.software.pool',
